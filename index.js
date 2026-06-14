d54902981f427967bff65371c1f6e68de5a72638
Juan Camilo Castellanos Uribe <juancamilo891018@gmail.com>
2026-06-14 16:46:24 -0500

    feat: add error handler middleware

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const postsRouter = require("./src/routes/posts");
 app.use("/authors", authorsRouter);
 app.use("/posts", postsRouter);
 
+const errorHandler = require("./src/middlewares/errorHandler");
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
